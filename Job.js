24b0e0a58d84869eab575634f31217e35a6993d1
Alex Yan <frickenhamster@github.com>
2016-08-15 14:22:15 -0700

    wip

--- Job.js
+++ Job.js
@@ -3,15 +3,14 @@
  */
 
 let COMMODITY_IDS = require('./Commodity').COMMODITY_IDS;
+let Commodity = require('./Commodity').Commodity;
 
 const JobIDs = {
 	FARM: 0
 };
 
-class Job
-{
-	constructor(workerLimit)
-	{
+class Job {
+	constructor(workerLimit) {
 	}
 }
 
@@ -21,26 +20,22 @@ const PROCESS_IDS = {
 };
 
 
-class ProcessType
-{
-	constructor(inputs, outputs, time)
-	{
+class ProcessType {
+	constructor(inputs, outputs, time) {
 		this.inputs = inputs;
 		this.outputs = outputs;
-		this.time = time;
+		this.work = work;
 	}
 }
 
 let PROCESS_CODEX = {};
 
-function addProcess(id, inputs, outputs, time)
-{
-	PROCESS_CODEX[id] = new ProcessType(inputs, outputs, time)
+function addProcess(id, inputs, outputs, work) {
+	PROCESS_CODEX[id] = new ProcessType(inputs, outputs, work)
 }
 
-function initProcesses()
-{
-	addProcess(PROCESS_IDS.FARM_APPLE, [], [COMMODITY_IDS.APPLE], 20);
+function initProcesses() {
+	addProcess(PROCESS_IDS.FARM_APPLE, [], [{type: COMMODITY_IDS.APPLE, amount: 10}], 20);
 }
 
 const PROCESS_STATE = {
@@ -49,13 +44,36 @@ const PROCESS_STATE = {
 	WORKING: 2,
 };
 
-class Process
-{
-	constructor(type)
-	{
+class Process {
+	constructor(type, city) {
 		this.type = type;
+		this.city;
 		this.state = PROCESS_STATE.LOADING_INPUTS;
+		this.codexEntry = PROCESS_CODEX[type];
+		this.inputsRequired = {};
+		for (let i = 0; i < this.codexEntry.inputs.length; i++) {
+
+		}
 	}
+
+	resetInputs() {
+
+	}
+
+	addCommodities(commodities) {
+
+	}
+
+	finishCycle() {
+		for (const outputStruct of this.codexEntry.outputs) {
+			const newCom = new Commodity({
+				type: outputStruct.type,
+				amount: outputStruct.amount
+			},);
+			this.city.addCommodities(newCom);
+		}
+	}
+
 }
 
 
